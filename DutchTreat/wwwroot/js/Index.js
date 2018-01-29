ee05fc42ff784200a3feaf87ef5b160f9cd47942
Harold Leonardo Ardila Corredor <hleonard31@gmail.com>
2018-01-29 11:36:14 -0500

    Uso de JQuery y JS

---
+++ DutchTreat/wwwroot/js/Index.js
@@ -0,0 +1,20 @@
+﻿var x = 0;
+var s = "";
+
+console.log("Hola Pluralsight");
+
+var theForm = document.getElementById("form");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    if (theForm.hidden == true) {
+        theForm.hidden = false;
+    }
+    else {
+        theForm.hidden = true;
+    }
+});
+
+var informacionProducto = document.getElementsByClassName("informacion-producto");
+var listItems = informacionProducto.item[0].children;
